7476572b84bfc655ec9fa8b5635e9d210c466bbd
Laurin <rungglaurin@gmail.com>
2023-04-19 09:24:16 +0200

    User-Klasse & Workout

---
+++ src/main/java/com/example/fitnesstp/FitnessApp.java
@@ -0,0 +1,5 @@
+package com.example.fitnesstp;
+
+
+public class FitnessApp {
+}
---
+++ src/main/java/com/example/fitnesstp/User.java
@@ -0,0 +1,74 @@
+package com.example.fitnesstp;
+
+import java.util.List;
+
+/**
+ * User - Class
+ * @
+ */
+
+public class User {
+    private String name;
+    private int age;
+    private Double height;
+    private Double weight;
+    private char gender;
+    private List<Workout> workouts;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public List<Workout> getWorkouts() {
+        return workouts;
+    }
+
+    public void setWorkouts(List<Workout> workouts) {
+        this.workouts = workouts;
+    }
+
+    public void addWorkout(Workout workoutToAdd){
+        workouts.add(workoutToAdd);
+    }
+
+    public void removeWorkout(Workout workoutToRemove){
+        workouts.remove(workoutToRemove);
+    }
+}
