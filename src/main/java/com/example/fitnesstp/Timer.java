a40559ccb378aadf339264be1179e2d189f7e0e2
PunDom <123359136+PunDom@users.noreply.github.com>
2023-04-19 08:01:53 +0200

    Klassen

---
+++ src/main/java/com/example/fitnesstp/Timer.java
@@ -0,0 +1,18 @@
+package com.example.fitnesstp;
+
+import javafx.scene.control.Label;
+
+/**
+ *
+ */
+
+public class Timer {
+
+    private final Label label;
+    public Timer(Label label) {
+        this.label = label;
+    }
+    public void startTimer(Long hours, Long minutes, long seconds){
+
+    }
+}
---
+++ src/main/java/com/example/fitnesstp/Workout.java
@@ -0,0 +1,4 @@
+package com.example.fitnesstp;
+
+public class Workout {
+}
