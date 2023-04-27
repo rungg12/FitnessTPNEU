4d177450ac7d6f47d016310544019d07abb942b3
PunDom <123359136+PunDom@users.noreply.github.com>
2023-04-27 12:29:36 +0200

    Media Klasse zu gian gebrocht

--- src/main/java/com/example/fitnesstp/Exercise.java
+++ src/main/java/com/example/fitnesstp/Exercise.java
@@ -1,4 +1,21 @@
 package com.example.fitnesstp;
 
+import javafx.scene.media.*;
+
+
 public class Exercise {
+    private final String name;
+    private final Media exampleVideo;
+
+    public Exercise(String name, Media exampleVideo) {
+        this.name = name;
+        this.exampleVideo = exampleVideo;
+    }
+
+    public Media getExampleVideo(){
+        return this.exampleVideo;
+    }
+    public String getName(){
+        return this.name;
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.fitnesstp {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires javafx.media;
 
     opens com.example.fitnesstp to javafx.fxml;
     exports com.example.fitnesstp;
