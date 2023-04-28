e34796a18ba1c32e8d07b68376f366c9b4f8beff
Laurin <rungglaurin@gmail.com>
2023-04-28 09:47:37 +0200

    Design Homepage und dazugehoerige CSS

--- src/main/java/com/example/fitnesstp/HelloApplication.java
+++ src/main/java/com/example/fitnesstp/HelloApplication.java
@@ -2,26 +2,40 @@ package com.example.fitnesstp;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.util.Objects;
 
 public class HelloApplication extends Application {
-    @Override
+    /*@Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("FXML-Login-Register.fxml"));
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Homepage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
 
-        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("Style.css")).toString());
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("HomepageStyle.css")).toString());
 
         stage.setTitle("Fitness-Programm");
         stage.setScene(scene);
+        stage.initStyle(StageStyle.DECORATED);
         stage.show();
+        */
+        @Override
+        public void start(Stage primaryStage) throws IOException {
+                //BorderPane root = new BorderPane();
+                Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("FXML-Login-Register.fxml")));
+                Scene scene = new Scene(root,1920,1080);
+                scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("Style.css")).toExternalForm());
+                primaryStage.setScene(scene);
+                primaryStage.initStyle(StageStyle.DECORATED);
+                primaryStage.setFullScreen(false);
+                primaryStage.show();
+        }
 
-
-    }
     public static void main(String[] args) {
         launch();
     }
---
+++ src/main/java/com/example/fitnesstp/HomepageController.java
@@ -0,0 +1,16 @@
+package com.example.fitnesstp;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+
+public class HomepageController {
+    public Pane background;
+
+    public void initialize(){
+
+    }
+
+    public void test(){
+        System.out.println(background.getLayoutX() + " " + background.getLayoutY() + " " + background.getHeight() + " " + background.getWidth());
+    }
+}
