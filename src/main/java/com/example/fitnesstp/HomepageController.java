320faf575f84362f4389481ccf6c3a2dc9ec6c30
Laurin <rungglaurin@gmail.com>
2023-05-05 10:30:16 +0200

    HomepageController: Die Grafik fertiggestellt

--- src/main/java/com/example/fitnesstp/HomepageController.java
+++ src/main/java/com/example/fitnesstp/HomepageController.java
@@ -3,14 +3,14 @@ package com.example.fitnesstp;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.transform.Scale;
 
 import java.util.Objects;
 
 public class HomepageController {
     Scene sceneAct;
     Pane background;
-
-    public HomepageController(Scene scene, Pane root) {
+    public HomepageController(Scene scene, Pane root){
         root.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/Homestyle.css")).toExternalForm());
 
         sceneAct = scene;
@@ -19,34 +19,50 @@ public class HomepageController {
         background.setId("background");
 
         Rectangle siteBar = new Rectangle();
-        siteBar.setX(sceneAct.getWidth() * 0.0156);
-        siteBar.setY(sceneAct.getHeight() * 0.0278);
-        siteBar.setWidth(sceneAct.getWidth() * 0.1);
-        siteBar.setHeight(sceneAct.getHeight() * 0.95);
+        siteBar.setX(sceneAct.getWidth()*0.0156);
+        siteBar.setY(sceneAct.getHeight()*0.0278);
+        siteBar.setWidth(sceneAct.getWidth()*0.1);
+        siteBar.setHeight(sceneAct.getHeight()*0.95);
         siteBar.setId("RectPurple");
 
         Rectangle mainView = new Rectangle();
-        mainView.setX(siteBar.getX() + siteBar.getWidth() + sceneAct.getWidth() * 0.04);
-        mainView.setY(sceneAct.getHeight() * 0.0278);
-        mainView.setWidth(sceneAct.getWidth() - mainView.getX() - sceneAct.getWidth() * 0.02);
-        mainView.setHeight(sceneAct.getHeight() * 0.5);
+        mainView.setX(siteBar.getX()+siteBar.getWidth() + sceneAct.getWidth()*0.04);
+        mainView.setY(sceneAct.getHeight()*0.0278);
+        mainView.setWidth(sceneAct.getWidth()-mainView.getX() - sceneAct.getWidth()*0.02);
+        mainView.setHeight(sceneAct.getHeight()*0.5);
         mainView.setId("RectLightpurple");
 
+        double sliderWidth = mainView.getWidth()/4 - mainView.getWidth()*0.02;
+        double sliderSpace = (mainView.getWidth() - 4*sliderWidth)/3;
+
         Rectangle slider1 = new Rectangle();
-        slider1.setX(siteBar.getX() + siteBar.getWidth() + sceneAct.getWidth() * 0.04);
-        slider1.setY(mainView.getY() + mainView.getHeight() + sceneAct.getHeight() * 0.04);
-        slider1.setWidth(sceneAct.getWidth() * 0.156);
-        slider1.setHeight(sceneAct.getHeight() * 0.278);
+        slider1.setX(mainView.getX());
+        slider1.setY(mainView.getY()+mainView.getHeight() + sceneAct.getHeight()*0.04);
+        slider1.setWidth(sliderWidth);
+        slider1.setHeight((siteBar.getY()+siteBar.getHeight()) - slider1.getY());
         slider1.setId("slider");
 
         Rectangle slider2 = new Rectangle();
-        slider2.setX(siteBar.getX() + siteBar.getWidth() + slider1.getWidth() + sceneAct.getWidth() * 0.04 * 2);
-        slider2.setY(mainView.getY() + mainView.getHeight() + sceneAct.getHeight() * 0.04);
-        slider2.setWidth(sceneAct.getWidth() * 0.156);
-        slider2.setHeight(sceneAct.getHeight() * 0.278);
+        slider2.setX(mainView.getX() + sliderWidth + sliderSpace);
+        slider2.setY(mainView.getY()+mainView.getHeight() + sceneAct.getHeight()*0.04);
+        slider2.setWidth(sliderWidth);
+        slider2.setHeight((siteBar.getY()+siteBar.getHeight()) - slider2.getY());
         slider2.setId("slider");
 
-        background.getChildren().addAll(siteBar, mainView, slider1, slider2);
+        Rectangle slider3 = new Rectangle();
+        slider3.setX(mainView.getX() + sliderWidth*2 + sliderSpace*2);
+        slider3.setY(mainView.getY()+mainView.getHeight() + sceneAct.getHeight()*0.04);
+        slider3.setWidth(sliderWidth);
+        slider3.setHeight((siteBar.getY()+siteBar.getHeight()) - slider3.getY());
+        slider3.setId("slider");
+
+        Rectangle slider4 = new Rectangle();
+        slider4.setX(mainView.getX() + sliderWidth*3 + sliderSpace*3);
+        slider4.setY(mainView.getY()+mainView.getHeight() + sceneAct.getHeight()*0.04);
+        slider4.setWidth(sliderWidth);
+        slider4.setHeight((siteBar.getY()+siteBar.getHeight()) - slider4.getY());
+        slider4.setId("slider");
+
+        background.getChildren().addAll(siteBar, mainView, slider1, slider2, slider3, slider4);
     }
 }
-
