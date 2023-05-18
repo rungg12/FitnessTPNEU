9a3a0e6fd97908a49652881c5c26fe624bb42e01
Laurin <rungglaurin@gmail.com>
2023-05-18 08:47:08 +0200

    Bilder neu eingefügt

--- src/main/java/com/example/fitnesstp/HelloApplication.java
+++ src/main/java/com/example/fitnesstp/HelloApplication.java
@@ -3,43 +3,41 @@ package com.example.fitnesstp;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
 
-    public void start(Stage stage) throws InterruptedException {
+    @Override
+    public void start(Stage stage) {
 
         int windowWidth = 1920;
         int windowHeight = 1080;
 
         Pane root = new Pane();
-        root.setPrefSize(windowWidth, windowHeight);
+        root.setPrefSize(windowWidth,windowHeight);
         Scene scene = new Scene(root, windowWidth, windowHeight);
 
-        stage.setTitle("Fitness Gott");
 
+
+        stage.setTitle("Test");
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(scene);
         stage.setWidth(1920);
         stage.setHeight(1080);
 
-        stage.setScene(scene);
 
         stage.requestFocus();
-        //stage.setRenderScaleX(1);
-        //stage.setRenderScaleY(1);
-        //stage.setForceIntegerRenderScale(true);
-        //stage.setMaximized(true);
-        //stage.setFullScreen(true);
+        stage.setRenderScaleX(1);
+        stage.setRenderScaleY(1);
+        stage.setForceIntegerRenderScale(true);
+        stage.setMaximized(true);
         stage.show();
 
-        //HomepageController homepageController = new HomepageController(scene, root);
-        //LoginController loginController = new LoginController(scene, root, homepageController);
-        WorkoutController workoutController = new WorkoutController(scene, root);
-
-
-
+        HomepageController homepageController = new HomepageController(scene, root);
+        LoginController loginController = new LoginController(scene, root, homepageController);
 
     }
 
